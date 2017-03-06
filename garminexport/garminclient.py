41e8e276383f12e90fc1533dd3e75b06996d565c
Alessandro De Noia <alessandro@sdonklabs.com>
2017-03-06 22:28:12 +0000

    fix elif

--- garminexport/garminclient.py
+++ garminexport/garminclient.py
@@ -310,7 +310,7 @@ class GarminClient(object):
         #response = self.session.get("https://connect.garmin.com/proxy/activity-service-1.1/gpx/activity/{}?full=true".format(activity_id))
         if response.status_code in (404, 204):
             return None
-        elif response.status_code != 200:
+        if response.status_code != 200:
             raise Exception(u"failed to fetch GPX for activity {}: {}\n{}".format(
                 activity_id, response.status_code, response.text))        
         return response.text
