a6c9d7a6fac955a1cc21c863c34f06838fc1ed78
kfollesdal <kristoffer.follesdal@bkk.no>
2021-02-17 21:04:00 +0100

    Add from_date and to_date args to incremental_backup

--- garminexport/incremental_backup.py
+++ garminexport/incremental_backup.py
@@ -2,8 +2,8 @@
 import getpass
 import logging
 import os
-from datetime import timedelta
-from typing import List
+from datetime import timedelta, datetime
+from typing import List, Optional
 
 import garminexport.backup
 from garminexport.backup import supported_export_formats
@@ -13,12 +13,25 @@ from garminexport.retryer import Retryer, ExponentialBackoffDelayStrategy, MaxRe
 log = logging.getLogger(__name__)
 
 
+def _filter_by_date(from_date, to_date, start):
+    if (from_date is None) & (to_date is None):
+        return True
+    elif (from_date is not None) & (to_date is not None):
+        return (start.date() >=  from_date) & (start.date() <= to_date)
+    elif from_date is None:
+        return start.date() <= to_date
+    elif to_date is None:
+        return start.date() >= from_date
+
+
 def incremental_backup(username: str,
                        password: str = None,
                        backup_dir: str = os.path.join(".", "activities"),
                        export_formats: List[str] = None,
                        ignore_errors: bool = False,
-                       max_retries: int = 7):
+                       max_retries: int = 7,
+                       from_date: Optional[str] = None,
+                       to_date: Optional[str] = None):
     """Performs (incremental) backups of activities for a given Garmin Connect account.
 
     :param username: Garmin Connect user name
@@ -30,6 +43,8 @@ def incremental_backup(username: str,
     :param max_retries: The maximum number of retries to make on failed attempts to fetch an activity.
     Exponential backoff will be used, meaning that the delay between successive attempts
     will double with every retry, starting at one second. Default: 7.
+    :param from_date: Optional lower bound for activity start date. Format yyyymmdd. Comparison in UTC.
+    :param to_date: Optional upper bound for activity start date. Format yyyymmdd. Comparison in UTC.
 
     The activities are stored in a local directory on the user's computer.
     The backups are incremental, meaning that only activities that aren't already
@@ -44,6 +59,12 @@ def incremental_backup(username: str,
 
     if not password:
         password = getpass.getpass("Enter password: ")
+
+    if from_date is not None:
+        from_date = datetime.strptime(from_date, "%Y%m%d").date()
+
+    if to_date is not None:
+        to_date = datetime.strptime(to_date, "%Y%m%d").date()
 
     # set up a retryer that will handle retries of failed activity downloads
     retryer = Retryer(
@@ -61,6 +82,14 @@ def incremental_backup(username: str,
         log.info("%s contains %d backed up activities", backup_dir, len(backed_up))
 
         log.info("activities that aren't backed up: %d", len(missing_activities))
+
+        if (from_date is not None) or (to_date is not None):
+            log.info("filter activities by start dates in [%s, %s]", from_date, to_date)
+            missing_activities = list(filter(
+                lambda x: _filter_by_date(from_date, to_date, x[1])
+                , missing_activities
+            ))
+            log.info("after start date filtering %s activities aren't backed up", len(missing_activities))
 
         for index, activity in enumerate(missing_activities):
             id, start = activity
